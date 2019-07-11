c0494eb2a887fe9596f94a2e0e0442ad9aa1585d
KoHanjun <rhgkswns@naver.com>
2019-07-11 13:43:23 +0900

    버튼 추가.

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QAction, qApp
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 class MyApp(QWidget):
 
     def __init__(self):
         super().__init__()
-
         self.initUI()
 
     def initUI(self):
+
+        btn = QPushButton('Quit', self)
+        btn.move(50,50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.on_click)
+
+        # btn.click(QCoreApplication.instance().quit())
 
         self.setWindowTitle('My First Application')
         self.move(300, 300)
         self.resize(400, 200)
         self.show()
 
+    def on_click(self):
+        print('PyQt5 button click')
+        QCoreApplication.instance().quit()
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
-
-print("Test")
